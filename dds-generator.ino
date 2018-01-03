887f8e246ddd61e3d8f430c326ab69b673ba0474
Dmitry Khaleev <dkhaleev@gmail.com>
2018-01-04 00:31:28 +0200

    LCD test

--- dds-generator.ino
+++ dds-generator.ino
@@ -1,4 +1,36 @@
-#include <LCD5110_SSVS.h>
-extern uint8_t SmallFont[];
-extern uint8_t MediumNumbers[];
-LCD5110 lcd(10,11,12,13);
+/*
+1. русские буквы вводим английскими т.е. слово "Всего:" мы пишем как "D[jl&"
+2. для отображения переменнвх типа byte, int, long (цифровых короче) сначала преобразуем их 
+   в тип String
+*/
+#include <LCD5110_SSVS.h>//CLK, DIN, DC, RST
+
+LCD5110 myGLCD(10, 11, 12, 13); //запускаем LCD
+extern uint8_t RusFont[];  //подключаем русский шрифт
+
+#define light_pin 8 //пин подсветки LCD
+#define LCD_ON HIGH //включение подсветки (HIGH для синих 5110, LOW для красных)
+
+unsigned int balance = 45600;
+unsigned int moneyBill = 5000;
+unsigned int moneyCoin = 350;
+String rej[4] = {"otlhsq", "yjhvf", ";flysq", "lfhjv"}; //режимы: щедрый, норма, жадный, даром
+
+void setup() {
+  pinMode (light_pin, OUTPUT);      //настраиваем порт подсветки внут. дисплея
+  digitalWrite (light_pin, LCD_ON); //включаем подсветку
+  myGLCD.InitLCD(55);          //запуск LCD контраст 65
+  myGLCD.setFont(RusFont);   //устанавливаем русский шрифт
+}
+
+
+void loop() {
+
+  myGLCD.print("D[jl&", LEFT, 0); //всего
+  myGLCD.print("Cvtyf&", LEFT, 8); //смена
+  myGLCD.print("Htcehc&", LEFT, 24); //ресурс
+  myGLCD.print("Ht;bv&", LEFT, 32); //режим
+  myGLCD.print(String(balance), RIGHT, 0); //всего
+  myGLCD.print(String(moneyBill) + " " + String(moneyCoin), RIGHT, 8); //смена
+  myGLCD.print(rej[1], RIGHT, 32); //режим
+}
